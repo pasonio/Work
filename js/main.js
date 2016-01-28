e6096c13074540122b9c3419c70b1a2e416e33eb
pasonio <panasenko708@gmail.com>
2016-01-28 22:27:44 +0200

    del

--- js/main.js
+++
@@ -1,7 +0,0 @@
- (function( $ ) {
-	// Inside of this function, $() will work as an alias for jQuery ()
-	// and other libraries also using $ will not be accessible under this shortcut
-	$( document ).ready(function() {
-		alert("test");
-	});
-} )( jQuery );
