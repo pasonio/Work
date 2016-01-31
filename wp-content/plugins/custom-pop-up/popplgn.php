ca499f8d9bd9e5e9a8b17ade9b964e760a9067be
pasonio <panasenko708@gmail.com>
2016-02-01 01:44:29 +0200

    adding fixed admin page

--- wp-content/plugins/custom-pop-up/popplgn.php
+++ wp-content/plugins/custom-pop-up/popplgn.php
@@ -1,68 +1,137 @@
 <?php
-/* Plugin name: Pop Up plugin
-*/
-function plugin_page() {
-    add_menu_page( 'Pop-up', 'Pop-up settings', 'manage_options', 'popplgn_page', 'popplgn_callback_func', '', 50 );
-}
-add_action( 'admin_menu', 'plugin_page' );
-function popplgn_api_init() {
-//    Add the section to settings media field so we can add our fields to it
-    add_settings_section(/*should be triggered 1 time*/
-        'popplgn_setting_section',
-        'Pop up menu',
-        'popplgn_callback_func',
-        'popplgn_page'
-    );
-    add_settings_field(/* Should create for all rows*/
-        'popplgn_title',
-        'Title',
-        'popplgn_body_field',
+function popplgn_page() {
+    add_menu_page( 
+        'Pop-up',
+        'Pop-up settings',
+        'manage_options',
         'popplgn_page',
-        'popplgn_setting_section'
-    );
-    register_setting( 'popplgn_page', 'popplgn_title' );
-}
-add_action('admin_init', 'popplgn_api_init' );
-/*----------------Call do_settings_section for data output----------------------*/
-function popplgn_settings() {
-    /*Install the option defaults*/
-    if( isset( $_POST['submit'] ) ) {
-        /*Creating default array of values*/
-        $defaults = array(
-            'title' => '',
-            'body' => '',
-            'delay' => '',
-            'close_btn' => '0',
-            'esc_btn' => '0',
-            'overlay_btn' => '0'
+        'popplgn_settings_content'
         );
-    }
 }
-/*Function for displaying description in the admin page*/
-function popplgn_callback_func() {
-    echo '<p>The main settings for pop-up menu plugin.</p>';
+add_action( 'admin_menu', 'popplgn_page' );
+
+function popplgn_database() {
+    $defaults = array(
+        'title' => 'Default Title',
+        'body' => 'Default body text',
+        'delay' => '10',
+        'close_btn' => '0',
+        'esc_btn' => '0',
+        'overlay_btn' => '0'
+    );
+    update_option( 'popplgn_options', $defaults );
 }
-/*Function for displaying title setting field*/
+add_action( 'plugins_loaded', 'popplgn_database' );
+function popplgn_fields_register() {
+
+    add_settings_section(
+        'popplgn_settings_section',
+        'Display Options',
+        'popplgn_display',
+        'popplgn_options'
+    );
+
+    add_settings_field(
+        'popplgn_title',
+        'Display title',
+        'popplgn_title_field',
+        'popplgn_options',
+        'popplgn_settings_section'
+    );
+
+    add_settings_field(
+        'popplgn_body',
+        'Display body',
+        'popplgn_body_field',
+        'popplgn_options',
+        'popplgn_settings_section'
+    );
+
+    add_settings_field(
+        'popplgn_delay',
+        'Time of delay',
+        'popplgn_delay_field',
+        'popplgn_options',
+        'popplgn_settings_section'
+    );
+
+    add_settings_field(
+        'popplgn_close_btn',
+        'Display close button',
+        'popplgn_close_btn_field',
+        'popplgn_options',
+        'popplgn_settings_section'
+    );
+
+    add_settings_field(
+        'popplgn_esc_btn',
+        'Close menu with "ESC"',
+        'popplgn_esc_btn_field',
+        'popplgn_options',
+        'popplgn_settings_section'
+    );
+
+    add_settings_field(
+        'popplgn_overlay',
+        'Close menu with overlay',
+        'popplgn_overlay_field',
+        'popplgn_options',
+        'popplgn_settings_section'
+    );
+
+    register_setting( 'popplgn_options','popplgn_options', 'popplgn_field_sanitize' );
+}
+add_action( 'admin_init', 'popplgn_fields_register' );
+
+function popplgn_field_sanitize( $option ) {
+    $option = sanitize_text_field( $option );
+    return $option;
+}
+function popplgn_display() {
+    $text = '<div class="wrap">';
+        $text .= '<h2>Pop-up plugin</h2>';
+        $text .= '<p>Main settings of the plugin</p>';
+    $text .='</div>';
+
+    echo $text;
+}
+function popplgn_settings_content() {
+    settings_errors(); ?>
+
+    <form method="POST" action="options.php">
+        <?php settings_fields( 'popplgn_options' );
+        do_settings_sections( 'popplgn_options' );
+        submit_button();?>
+    </form>
+<?php }
+
 function popplgn_title_field() {
-    echo '<input name="popplgn-title[asdasd]" type="text" value="' . get_option( "popplgn_title" ) . '"/>';
+    $options = get_option( 'popplgn_options' );
+    $value = $options['title'];
+    echo '<input name="popplgn-options[title]" id="popplgn_title" type="text" value="' . $value . '"/>';
 }
-/*Function for displaying body text setting field*/
 function popplgn_body_field() {
-    echo '<textarea name="popplgn-body"></textarea>';
+    $options = get_option( 'popplgn_options' );
+    $value = $options['body'];
+    echo '<textarea name="popplgn-options[body]" id="popplgn_body">' . $value . '</textarea>';
 }
-/*Function for displaying time of delay field*/
 function popplgn_delay_field() {
-    echo '<input name="popplgn-delay" type="text"/>';
+    $options = get_option( 'popplgn_options' );
+    $value = $options['delay'];
+    echo '<input name="popplgn-options[delay]" id="popplgn_delay" type="text" value="' . $value . '"/>';
 }
-/*Function for displaying close button setting field*/
 function popplgn_close_btn_field() {
-    echo '<input name="popplgn-close-btn" type="checkbox" value="0"/>';
+    $options = get_option( 'popplgn_options' );
+    $value = $options['close_btn'];
+    echo '<input name="popplgn-options[close_btn]" id="popplgn_close_btn" type="checkbox" value="0"' . checked( 1, $value, false ) . '/>';
 }
-/*Function for displaying  "Close by press Esc" option setting field*/
 function popplgn_esc_btn_field() {
-    echo '<input name="popplgn-esc-btn" type="checkbox" value="0"/>';
+    $options = get_option( 'popplgn_options' );
+    $value = $options['esc_btn'];
+    echo '<input name="popplgn-options[esc_btn]" id="popplgn_esc_btn" type="checkbox" value="0"' . checked( 1, $value, false ) . '/>';
 }
-/*Function for displaying "Close by clicking Overlay" option setting field*/
 function popplgn_overlay_field() {
-    echo '<input name="popplgn-overlay" type="checkbox" value="0"/>';
+    $options = get_option( 'popplgn_options' );
+    $value = $options['overlay_btn'];
+    echo '<input name="popplgn-options[overlay_btn]" id="popplgn_overlay" type="checkbox" value="0"' . checked( 1, $value, false ) . '/>';
 }
